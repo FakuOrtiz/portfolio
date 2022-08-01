f726e787fea5e5032f320e773de7ebe1d592455f
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-08-01 20:17:35 -0300

    try 2 fix font family

--- src/components/homeCard/Home.jsx
+++ src/components/homeCard/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div id='seccion1' className={styles.containerCard}>
         <div className={styles.card}>
             <div className={styles.containerName}>
-                <h1 className={styles.title}>¡Hola!, soy Facundo Ortiz</h1>
+                <h1 className={styles.title}>¡Hola! soy Facundo Ortiz</h1>
                 <p className={styles.subtitle}>Desarrollador web apasionado por la tecnología. Me encanta el front-end.</p>
             </div>
         </div>
