fdd7b7cabd657a25016f03cf7f0fdbc59f542f8f
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-08-01 21:11:23 -0300

    feat/add projects v1

--- src/components/projectsCard/Projects.jsx
+++ src/components/projectsCard/Projects.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./Projects.module.css";
 import GitHub from "../../svg/GitHubBlack";
 import Deploy from "../../svg/Deploy";
+import scriptMusic from "../../assets/scriptmusic.png";
+import rickAndMorty from "../../assets/rickandmorty.png";
+import appemon from "../../assets/appemon.png";
 
 const Projects = () => {
   return (
@@ -13,6 +16,109 @@ const Projects = () => {
             <div className={styles.line}></div>
           </div>
 
+          <div style={{ display: "flex" }}>
+            <div className={styles.containerScript}>
+              <div>
+                <h3>Script-Music</h3>
+              </div>
+              <div>
+                <img
+                  src={scriptMusic}
+                  alt="ScriptMusic"
+                  className={styles.scriptIMG}
+                />
+              </div>
+              <div>
+                <p>
+                  Aplicación mobile nativa sobre venta de instrumentos
+                  musicales.
+                </p>
+              </div>
+              <div className={styles.containerGitHubDeploy}>
+                <div className={styles.icon}>
+                  <a
+                    href="https://github.com/JaluGJ/ScriptMusic"
+                    target="_blank"
+                  >
+                    <GitHub />
+                  </a>
+                </div>
+                <div className={styles.icon}>
+                  <a
+                    href="https://mega.nz/file/yDYUwTYb#O6wwXOD-AttU_lwO4sa6Hz9LBInCJhjrQbJi1N__wmU"
+                    target="_blank"
+                  >
+                    <Deploy />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className={styles.containerScript}>
+              <div>
+                <h3>APPemon</h3>
+              </div>
+              <div>
+                <img src={appemon} alt="APPemon" className={styles.scriptIMG} />
+              </div>
+              <div>
+                <p>
+                  Aplicación web que permite ver, ordenar, filtrar, buscar y
+                  crear pokémons.
+                </p>
+              </div>
+              <div className={styles.containerGitHubDeploy}>
+                <div className={styles.icon}>
+                  <a
+                    href="https://github.com/FakuOrtiz/PI-Pokemon"
+                    target="_blank"
+                  >
+                    <GitHub />
+                  </a>
+                </div>
+                <div className={styles.icon}>
+                  <a href="https://appemon.vercel.app/" target="_blank">
+                    <Deploy />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className={styles.containerScript}>
+              <div>
+                <h3>Rick and Morty App</h3>
+              </div>
+              <div>
+                <img
+                  src={rickAndMorty}
+                  alt="Rick and Morty App"
+                  className={styles.scriptIMG}
+                />
+              </div>
+              <div>
+                <p>
+                  Aplicación web que permite ver y buscar personajes de Rick and
+                  Morty.
+                </p>
+              </div>
+              <div className={styles.containerGitHubDeploy}>
+                <div className={styles.icon}>
+                  <a
+                    href="https://github.com/FakuOrtiz/RickAndMortyApp"
+                    target="_blank"
+                  >
+                    <GitHub />
+                  </a>
+                </div>
+                <div className={styles.icon}>
+                  <a
+                    href="https://rickandmortyapp-chi.vercel.app/"
+                    target="_blank"
+                  >
+                    <Deploy />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
