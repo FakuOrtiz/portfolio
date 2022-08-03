b1e7d47e2de3ac7a79a2adb029be4ab2df663305
Facundo Ortiz <facundoagustinortiz@gmail.com>
2022-08-03 17:58:22 -0300

    update home screen

--- src/components/homeCard/Home.jsx
+++ src/components/homeCard/Home.jsx
@@ -1,16 +1,54 @@
 import React from "react";
 import styles from "./Home.module.css";
+import me from "../../assets/profileSinBG.png";
+import Mail from "../../svg/Mail";
+import Download from "../../svg/Download";
+import cv from "../../assets/Facundo Ortiz - CV.pdf";
 
 const Home = () => {
   return (
     <div id="seccion1" className={styles.containerCard}>
       <div className={styles.card}>
         <div className={styles.containerName}>
-          <h1 className={styles.title}>¡Hola! soy Facundo Ortiz</h1>
+          <h1 className={styles.title}>
+            ¡Hola! soy Facundo Ortiz<span className={styles.punto}>.</span>
+          </h1>
           <p className={styles.subtitle}>
             Desarrollador web apasionado por la tecnología. Me encanta el
             Front-end.
           </p>
+          <div className={styles.containerCVMail}>
+            <div>
+              <a href={cv} download className={styles.linkDownload}>
+                <div className={styles.containerDownload}>
+                  <div style={{ width: "20px" }}>
+                    <Download />
+                  </div>
+                  <div>
+                    <p style={{ marginLeft: "10px" }}>Descargar CV</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div>
+              <a
+                href="mailto:facundoagustinortiz@gmail.com"
+                className={styles.linkMail}
+              >
+                <div className={styles.containerMail}>
+                  <div style={{ width: "20px" }}>
+                    <Mail />
+                  </div>
+                  <div>
+                    <p style={{ paddingLeft: "10px" }}>¡Contáctame!</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className={styles.containerIMG}>
+          <img src={me} alt="Facundo Ortiz" className={styles.image} />
         </div>
       </div>
     </div>
